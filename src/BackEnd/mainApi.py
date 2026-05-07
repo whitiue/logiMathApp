e382333b819e00233145923d7e661cef2286f6d2
angel <ayael.ag1209@gmail.com>
2026-05-06 22:15:42 -0600

    [Changes in security and changes in public safety and order]Se agregaron nuevas cosas respecto a la seguridad para futuro estar en un servidor y no ser local y orden para una mejor vista

--- src/BackEnd/mainApi.py
+++ src/BackEnd/mainApi.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from models import SessionLocal, User, Quiz, Question, UserScore
+from BackEnd.models import SessionLocal, User, Quiz, Question, UserScore
 from datetime import datetime
 from pydantic import BaseModel
 
